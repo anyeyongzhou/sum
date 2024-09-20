9c64c71642d131197456602463bf152d24386b9c
wqy <w1354785752@163.com>
2024-09-21 00:02:16 +0800

    echarts

--- src/views/1/1-20/data.ts
+++ src/views/1/1-20/data.ts
@@ -1,40 +1,28 @@
-const API_BASE = '/online_user';
-
-const API_SUFFIXES = {
-	/** 在线用户实时流动 */
-	STREAM_SSE: '/user-activity-sse',
-	/** 省略其他接口 */
-};
-
 export class OnlineUserAPI {
-	/**
-	 * 在线用户实时数据展示
-	 */
-	static STREAM_SSE = {
-		endpoint: (token: string) => {
-			return `${import.meta.env.VITE_APP_API_URL}${API_BASE}${API_SUFFIXES.STREAM_SSE}?Authorization=${token}`;
-		},
-		permission: 'monitor:online-user:list',
-		chartOptions: (): any => {
-			return {
-				title: {
-					text: '在线用户统计', // 图表标题
-					left: 'center', // 标题居中显示
-				},
-				xAxis: {
-					type: 'category',
-					data: ['35s', '30s', '25s', '20s', '15s', '10s', '5s'], // 可以根据需要修改为时间段或其他表示
-				},
-				yAxis: {
-					type: 'value',
-				},
-				series: [
-					{
-						data: ['0', '0', '0', '0', '0', '0', '0'], // 数据
-						type: 'line',
-					},
-				],
-			};
-		},
-	};
+  /**
+   * 在线用户实时数据展示
+   */
+  static STREAM_SSE = {
+    chartOptions: (): any => {
+      return {
+        title: {
+          text: "在线用户统计", // 图表标题
+          left: "center", // 标题居中显示
+        },
+        xAxis: {
+          type: "category",
+          data: ["35s", "30s", "25s", "20s", "15s", "10s", "5s"], // 可以根据需要修改为时间段或其他表示
+        },
+        yAxis: {
+          type: "value",
+        },
+        series: [
+          {
+            data: ["0", "0", "0", "0", "0", "0", "0"], // 数据
+            type: "line",
+          },
+        ],
+      };
+    },
+  };
 }
--- src/views/1/1-20/hooks使用echarts.vue
+++ src/views/1/1-20/hooks使用echarts.vue
@@ -1,163 +1,179 @@
 <template>
-	<div class="home">
-		<div class="container">
-			<template v-if="flag">
-				<Title :orderNum="1" context="echarts" :size="1" :color="9" :wight="1" :backgroundColor="3" />
-				<div ref="chartRef" style="width: 100%; height: 400px"></div>
-			</template>
-			<template v-else>
-				<div class="code">
-					<pre><code>{{ indexFile }}</code></pre>
-				</div>
-			</template>
-		</div>
-		<div class="tip" v-marked:hl="markdownContent"></div>
-		<div class="button">
-			<el-button type="primary" @click="handleClick">{{ buttonContent }}</el-button>
-		</div>
-	</div>
+  <div class="home">
+    <div class="container">
+      <template v-if="flag">
+        <Title
+          :orderNum="1"
+          context="echarts"
+          :size="1"
+          :color="9"
+          :wight="1"
+          :backgroundColor="3"
+        />
+        <div ref="chartRef" style="width: 100%; height: 400px"></div>
+      </template>
+      <template v-else>
+        <div class="code">
+          <pre><code>{{ indexFile }}</code></pre>
+        </div>
+      </template>
+    </div>
+    <div class="tip" v-marked:hl="markdownContent"></div>
+    <div class="button">
+      <el-button type="primary" @click="handleClick">{{
+        buttonContent
+      }}</el-button>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { useECharts } from './useECharts.ts';
-import { OnlineUserAPI } from './data.ts';
-import { useRoute } from 'vue-router';
-import { marked } from 'marked';
-import 'highlight.js/styles/monokai-sublime.css';
+import { useECharts } from "./useEchart";
+import { OnlineUserAPI } from "./data";
+import { useRoute } from "vue-router";
+import { marked } from "marked";
+import "highlight.js/styles/monokai-sublime.css";
 
 const route = useRoute();
 const flag = ref(true);
-const buttonContent = ref('查看代码');
-const indexFile = ref('');
+const buttonContent = ref("查看代码");
+const indexFile = ref("");
 
 const handleClick = async () => {
-	flag.value = !flag.value;
-	buttonContent.value = flag.value ? '查看代码' : '返回';
+  flag.value = !flag.value;
+  buttonContent.value = flag.value ? "查看代码" : "返回";
 
-	if (!flag.value) {
-		const filePath = `./${route.meta.title}.vue?raw`;
-		try {
-			const module = await import(filePath);
-			indexFile.value = module.default;
-		} catch (error) {
-			console.error('加载文件错误:', error);
-		}
-	}
+  if (!flag.value) {
+    const filePath = `./${route.meta.title}.vue?raw`;
+    try {
+      const module = await import(filePath);
+      indexFile.value = module.default;
+    } catch (error) {
+      console.error("加载文件错误:", error);
+    }
+  }
 };
 
 const markdownContent = ref(null);
 onMounted(async () => {
-	const mdFileContent = await import('./code.md?raw');
-	markdownContent.value = marked(mdFileContent.default);
+  const mdFileContent = await import("./code.md?raw");
+  markdownContent.value = marked(mdFileContent.default);
 
-	setTimeout(() => {
-		options.series[0].data = [210, 200, 200, 200, 200, 220, 280];
-	}, 1000);
+  setTimeout(() => {
+    options.series[0].data = [210, 200, 200, 200, 200, 220, 280];
+  }, 1000);
 });
 
 const chartRef = ref(null);
-const { options } = useECharts(chartRef, OnlineUserAPI.STREAM_SSE.chartOptions());
+const { options } = useECharts(
+  chartRef,
+  OnlineUserAPI.STREAM_SSE.chartOptions()
+);
 </script>
 
 <style lang="scss" scoped>
 .home {
-	height: 110%;
-	width: 100%;
-	background-color: #fff;
-	position: relative;
-	overflow-y: scroll;
+  height: 110%;
+  width: 100%;
+  background-color: #fff;
+  position: relative;
+  overflow-y: scroll;
 
-	.container {
-		width: 60%;
-		height: 100%;
-		padding: 10px;
-		border: 1px solid black;
+  .container {
+    width: 60%;
+    height: 100%;
+    padding: 10px;
+    border: 1px solid black;
 
-		.face {
-			display: flex;
-			justify-content: center;
+    .face {
+      display: flex;
+      justify-content: center;
 
-			.face-inner {
-				width: 200px;
-				height: 200px;
-				border-radius: 50%;
-				background: linear-gradient(180deg, rgb(255, 207, 5), rgb(255, 240, 36));
-				position: relative;
-				top: 50px;
-				box-shadow: inset 0 0 3px rgb(15, 15, 9), inset 0 0 5px rgb(15, 15, 9);
-			}
+      .face-inner {
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+        background: linear-gradient(
+          180deg,
+          rgb(255, 207, 5),
+          rgb(255, 240, 36)
+        );
+        position: relative;
+        top: 50px;
+        box-shadow: inset 0 0 3px rgb(15, 15, 9), inset 0 0 5px rgb(15, 15, 9);
+      }
 
-			.face-inner::after {
-				content: '';
-				position: absolute;
-				bottom: 12%;
-				left: 50%;
-				transform: translateX(-50%);
-				width: 90px;
-				height: 40px;
-				background-color: rgb(255, 199, 45);
-				border-radius: 0 0 70px 70px;
-				box-shadow: inset 0 0 5px rgb(53, 53, 53);
-			}
+      .face-inner::after {
+        content: "";
+        position: absolute;
+        bottom: 12%;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 90px;
+        height: 40px;
+        background-color: rgb(255, 199, 45);
+        border-radius: 0 0 70px 70px;
+        box-shadow: inset 0 0 5px rgb(53, 53, 53);
+      }
 
-			.eye {
-				position: absolute;
-				width: 60px;
-				height: 60px;
-				border-radius: 50%;
-				background-color: #fff;
-				box-shadow: inset 0 0 5px rgb(58, 58, 58);
-			}
+      .eye {
+        position: absolute;
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        background-color: #fff;
+        box-shadow: inset 0 0 5px rgb(58, 58, 58);
+      }
 
-			.eye::after {
-				content: '';
-				position: absolute;
-				top: 50%;
-				left: 5%;
-				transform: translateY(-50%);
-				width: 50%;
-				height: 50%;
-				background-color: #000;
-				border-radius: 50%;
-				box-shadow: inset 0 0 3px white, inset 0 0 5px white;
-			}
+      .eye::after {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 5%;
+        transform: translateY(-50%);
+        width: 50%;
+        height: 50%;
+        background-color: #000;
+        border-radius: 50%;
+        box-shadow: inset 0 0 3px white, inset 0 0 5px white;
+      }
 
-			.eye:nth-child(1) {
-				top: 26%;
-				left: 12%;
-			}
+      .eye:nth-child(1) {
+        top: 26%;
+        left: 12%;
+      }
 
-			.eye:nth-child(2) {
-				top: 26%;
-				right: 12%;
-			}
-		}
+      .eye:nth-child(2) {
+        top: 26%;
+        right: 12%;
+      }
+    }
 
-		.code {
-			padding: 20px;
-			border: 1px solid gray;
-			font-size: 20px;
-		}
-	}
+    .code {
+      padding: 20px;
+      border: 1px solid gray;
+      font-size: 20px;
+    }
+  }
 
-	.tip {
-		width: 600px;
-		min-height: 100px;
-		max-height: 600px;
-		position: fixed;
-		right: 50px;
-		top: 200px;
-		overflow-y: scroll;
-		padding: 10px;
-		background-color: #e0e0e0;
-		box-shadow: 10px 10px 5px rgba(0, 0, 0, 0.5);
-		border-radius: 10px;
-	}
+  .tip {
+    width: 600px;
+    min-height: 100px;
+    max-height: 600px;
+    position: fixed;
+    right: 50px;
+    top: 200px;
+    overflow-y: scroll;
+    padding: 10px;
+    background-color: #e0e0e0;
+    box-shadow: 10px 10px 5px rgba(0, 0, 0, 0.5);
+    border-radius: 10px;
+  }
 
-	.button {
-		position: fixed;
-		right: 50px;
-		top: 130px;
-	}
+  .button {
+    position: fixed;
+    right: 50px;
+    top: 130px;
+  }
 }
 </style>
