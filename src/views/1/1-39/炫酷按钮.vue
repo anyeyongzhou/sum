5d00edea06ccef5f7f119ace72b95b4ca17ffcb1
wqy <w1354785752@163.com>
2024-09-25 00:19:41 +0800

    炫酷按钮

---
+++ src/views/1/1-39/炫酷按钮.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <template v-if="flag">
+        <Heads :context="route.meta.title" />
+        <MyButton>Button</MyButton>
+      </template>
+      <template v-else>
+        <div class="code">
+          <pre><code>{{ indexFile }}</code></pre>
+        </div>
+      </template>
+    </div>
+    <div class="button">
+      <el-button type="primary" @click="handleClick">{{
+        buttonContent
+      }}</el-button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import MyButton from "./components/MyButton.vue";
+import * as echarts from "echarts";
+import "echarts-liquidfill";
+/* 不改的代码 begin */
+import { useRoute } from "vue-router";
+import { marked } from "marked";
+import "highlight.js/styles/monokai-sublime.css";
+
+const route = useRoute();
+const flag = ref(true);
+const buttonContent = ref("查看代码");
+const indexFile = ref(""); // 用于存储动态加载的内容
+
+const handleClick = async () => {
+  flag.value = !flag.value;
+  buttonContent.value = flag.value ? "查看代码" : "返回";
+
+  if (!flag.value) {
+    // 动态加载 .vue 文件的原始内容
+    const filePath = `./${route.meta.title}.vue?raw`; // 根据实际路径设置
+    try {
+      const module = await import(filePath);
+      indexFile.value = module.default; // 读取原始代码
+    } catch (error) {
+      console.error("加载文件错误:", error);
+    }
+  }
+};
+
+/* 不改的代码 end */
+</script>
+
+<style lang="scss" scoped>
+.home {
+  height: 110%;
+  width: 100%;
+  background-color: #fff;
+  position: relative;
+  overflow-y: scroll;
+
+  .container {
+    width: 60%;
+    height: 100%;
+    padding: 10px;
+    border: 1px solid black;
+
+    /* 编写CSS begin */
+
+    /* 编写CSS end */
+
+    .code {
+      padding: 20px;
+      border: 1px solid gray;
+      font-size: 20px;
+    }
+  }
+
+  .tip {
+    width: 30%;
+    min-height: 100px;
+    max-height: 600px;
+    position: fixed;
+    right: 50px;
+    top: 200px;
+    overflow-y: scroll;
+    padding: 10px;
+    background-color: #e0e0e0;
+    box-shadow: 10px 10px 5px rgba(0, 0, 0, 0.5);
+    border-radius: 10px;
+  }
+
+  .button {
+    position: fixed;
+    right: 50px;
+    top: 130px;
+  }
+}
+</style>
