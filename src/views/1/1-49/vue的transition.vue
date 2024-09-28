4266259f6351d493d3baaa15eb5c0eb6768ccd28
wqy <w1354785752@163.com>
2024-09-29 00:02:08 +0800

    vue的transition

---
+++ src/views/1/1-49/vue的transition.vue
@@ -0,0 +1,34 @@
+<template>
+  <button @click="show = !show">Toggle</button>
+  <transition name="fade">
+    <div
+      v-if="show"
+      style="border: 1px solid red; height: 200px; background-color: #000"
+    >
+      <div
+        style="border: 1px solid green; height: 100px; background-color: #fff"
+      >
+        556
+      </div>
+    </div>
+  </transition>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
+</script>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
