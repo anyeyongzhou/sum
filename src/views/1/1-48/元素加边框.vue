f5efe2d7601d2dd148815b3d76126b7e4ff292e9
wqy <w1354785752@163.com>
2024-09-28 23:27:02 +0800

    元素加边框

---
+++ src/views/1/1-48/元素加边框.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <template v-if="flag">
+        <Heads :context="route.meta.title" />
+        <el-button type="primary" @click="addBorder">加边框</el-button>
+        <el-button type="primary" @click="removeBorder">去除边框</el-button>
+      </template>
+      <template v-else>
+        <div class="code">
+          <pre><code>{{ indexFile }}</code></pre>
+        </div>
+      </template>
+    </div>
+    <div class="tip" v-marked:hl="markdownContent"></div>
+    <div class="button">
+      <el-button type="primary" @click="handleClick">{{
+        buttonContent
+      }}</el-button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+/* 不改的代码 begin */
+import { useRoute } from "vue-router";
+import { marked } from "marked";
+import "highlight.js/styles/monokai-sublime.css";
+
+const route = useRoute();
+const flag = ref(true);
+const buttonContent = ref("查看代码");
+const indexFile = ref(""); // 用于存储动态加载的内容
+
+const handleClick = async () => {
+  flag.value = !flag.value;
+  buttonContent.value = flag.value ? "查看代码" : "返回";
+
+  if (!flag.value) {
+    // 动态加载 .vue 文件的原始内容
+    const filePath = `./${route.meta.title}.vue?raw`; // 根据实际路径设置
+    try {
+      const module = await import(filePath);
+      indexFile.value = module.default; // 读取原始代码
+    } catch (error) {
+      console.error("加载文件错误:", error);
+    }
+  }
+};
+
+const markdownContent = ref(null);
+onMounted(async () => {
+  const mdFileContent = await import("./code.md?raw");
+  const replaceUrl = `../src/views/${route.name.split("-").at(0)}/${
+    route.fullPath
+  }`;
+  const markdownText = marked(mdFileContent.default);
+  markdownContent.value = markdownText
+    .replace(/src="(images\/code\/\d+\.png)"/, `src="${replaceUrl}/$1"`)
+    .replace(/(<img[^>]*)(>)/, '$1 style="width:100%;height:100%;"$2');
+});
+/* 不改的代码 end */
+const addBorder = () => {
+  [].forEach.call(document.querySelectorAll("*"), function (a) {
+    a.style.border =
+      "1px solid #" + (~~(Math.random() * (1 << 24))).toString(16);
+  });
+};
+
+const removeBorder = () => {
+  [].forEach.call(document.querySelectorAll("*"), function (a) {
+    a.style.border = "none";
+  });
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  height: 110%;
+  width: 100%;
+  background-color: #fff;
+  position: relative;
+  overflow-y: scroll;
+
+  .container {
+    width: 60%;
+    height: 100%;
+    padding: 10px;
+    border: 1px solid black;
+
+    /* 编写CSS begin */
+
+    /* 编写CSS end */
+
+    .code {
+      padding: 20px;
+      border: 1px solid gray;
+      font-size: 20px;
+    }
+  }
+
+  .tip {
+    width: 30%;
+    min-height: 100px;
+    max-height: 600px;
+    position: fixed;
+    right: 50px;
+    top: 200px;
+    overflow-y: scroll;
+    padding: 10px;
+    background-color: #e0e0e0;
+    box-shadow: 10px 10px 5px rgba(0, 0, 0, 0.5);
+    border-radius: 10px;
+  }
+
+  .button {
+    position: fixed;
+    right: 50px;
+    top: 130px;
+  }
+}
+</style>
