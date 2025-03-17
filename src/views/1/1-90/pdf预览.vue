2bff6826ab5298444be9e768fad54964c62f4594
wqy <w1354785752@163.com>
2025-03-17 16:55:46 +0800

    pdf预览

---
+++ src/views/1/1-90/pdf预览.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="home">
+		<div class="containers">
+			<VuePdfEmbed annotation-layer text-layer :source="pdfSource" />
+		</div>
+	</div>
+</template>
+
+<script setup>
+import VuePdfEmbed from 'vue-pdf-embed';
+import aaa from './aaa.pdf';
+
+// optional styles
+import 'vue-pdf-embed/dist/styles/annotationLayer.css';
+import 'vue-pdf-embed/dist/styles/textLayer.css';
+
+// either URL, Base64, binary, or document proxy
+const pdfSource = aaa;
+</script>
+
+<style lang="scss" scoped>
+.home {
+	height: 100%;
+	width: 100%;
+	background-color: #fff;
+	position: relative;
+	overflow-y: scroll;
+
+	.containers {
+		width: 100%;
+		height: 100%;
+		padding: 10px;
+		border: 1px solid black;
+	}
+}
+</style>
